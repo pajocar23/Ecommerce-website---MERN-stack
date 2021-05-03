35cf0e355d104277b57f7ecb9ed40064962e785f
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-05-03 18:49:15 +0200

    Get single product by ID

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,9 +2,11 @@ const express=require('express');
 const app = require('../app');
 const router=express.Router();
 
-const{ getProducts, newProduct }=require('../controllers/productController')
+const{ getProducts, newProduct, getSingleProduct }=require('../controllers/productController')
 
 router.route('/products').get(getProducts);
+
+router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
 
