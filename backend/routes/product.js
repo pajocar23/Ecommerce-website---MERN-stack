b90b8d0f8035538f07887f8235bb3c6cf06ad0ad
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-05-03 19:01:05 +0200

    Update a product
    Api for updating product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,13 +2,15 @@ const express=require('express');
 const app = require('../app');
 const router=express.Router();
 
-const{ getProducts, newProduct, getSingleProduct }=require('../controllers/productController')
+const{ getProducts, newProduct, getSingleProduct,updateProduct }=require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/product/new').post(newProduct);
+router.route('/admin/product/new').post(newProduct);
+
+router.route('/admin/product/:id').put(updateProduct);
 
 module.exports=router;
 
