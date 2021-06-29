7ae1f8dbd8ce09586f9b1946afcbced8aed997b4
pajocar23 <56831069+pajocar23@users.noreply.github.com>
2021-06-29 12:59:17 +0200

    admin auth and forgot pass token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,60 +2,111 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
+const sendEmail=require('../utils/sendEmail')
 
 //Register a user => /api/v1/register
-exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
-    const{ name, email, password }=req.body;
+    const { name, email, password } = req.body;
 
     const user = await User.create({
         name,
         email,
         password,
-        avatar:{
-            public_id:'avatars/PNG_transparency_demonstration_2_xmmyng',
+        avatar: {
+            public_id: 'avatars/PNG_transparency_demonstration_2_xmmyng',
             url: 'https://res.cloudinary.com/dmects8t7/image/upload/v1620817580/avatars/PNG_transparency_demonstration_2_xmmyng.png'
         }
     })
 
-
-    const token=user.getJwtToken();
-
-    res.status(201).json({
-        success:true,
-        token
-    })
+    sendToken(user, 200, res)
 
 })
 
 
 //Login User =>/a[i/v1/login
-exports.loginUser=catchAsyncErrors( async(req,res,next)=>{
-    const {email, password}=req.body;
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
 
     //Checks if email and password is entered by user
-    if(!email || !password){
-        return next(new ErrorHandler('Please enter email & password',400))
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email & password', 400))
     }
 
     //Finding user in database
-    const user=await User.findOne({email}).select('+password')
+    const user = await User.findOne({ email }).select('+password')
 
-    if(!user){
-        return next(new ErrorHandler('Invalid Email or Password',401));
+    if (!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
     //Checks if password is correct or not
-    const isPasswordMatched=await user.comparePassword(password);
+    const isPasswordMatched = await user.comparePassword(password);
 
-    if(!isPasswordMatched){
-        return next(new ErrorHandler('Invalid Email or Password',401)); 
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
     }
 
-    const token=user.getJwtToken();
+    sendToken(user, 200, res)
+})
+
+//Forgot password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    //Get reser token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false })
+
+    //Create reset password url
+    const resetUrl=`${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+
+    const message=`Your password reset token is as follows:\n\n${resetUrl}\n\nIf you have not
+    requested this email, then ignore it.`
+
+    try{
+
+        await sendEmail({
+            email:user.email,
+            subject:'ShopIT Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success:true,
+            message: `Email sent to : ${user.email}`
+        })
+
+    }catch(error){
+        user.resetPasswordToken=undefined;
+        user.resetPasswordExpire=undefined;
+
+        await user.save({ validateBeforeSave: false});
+
+        return next(new ErrorHandler(error.message,500))
+    }
+
+})
+
+
+//Logouut user => /api/v1/logout
+
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+    res.cookie('token', null, {
+        expiress: new Date(Date.now()),
+        httpOnly: true
+    })
+
 
     res.status(200).json({
-        success:true,
-        token
+        success: true,
+        message: "Logged out"
     })
 })
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
+
 
 const userSchema = new mongoose.Schema({
     name: {
-        type:String,
-        required: [true,'Please enter your name'],
+        type: String,
+        required: [true, 'Please enter your name'],
         maxlength: [30, 'Your name cannot exceed 30 characters']
     },
     email: {
@@ -32,10 +34,10 @@ const userSchema = new mongoose.Schema({
         }
     },
     role: {
-        type:String,
+        type: String,
         default: 'user'
     },
-    createdAt:{
+    createdAt: {
         type: Date,
         default: Date.now
     },
@@ -45,23 +47,37 @@ const userSchema = new mongoose.Schema({
 })
 
 //Encrypting password before saving user
-userSchema.pre('save', async function(next){
-    if(!this.isModified('password')){
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
         next()
     }
     this.password = await bcrypt.hash(this.password, 10)
 })
 
 //Compare user password
-userSchema.methods.comparePassword=async function(enteredPassword){
+userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
 
 //Return JWT token
-userSchema.methods.getJwtToken=function(){
-    return jwt.sign({id: this._id}, process.env.JWT_SECRET,{
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_TIME
     });
+}
+
+//Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+    //Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //Hash and set to resetPasswordToken
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+
+    //Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+    return resetToken
 }
 
 module.exports = mongoose.model('User', userSchema)
