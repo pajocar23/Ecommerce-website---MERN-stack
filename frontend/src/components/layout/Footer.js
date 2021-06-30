9a3088e47a5105cedd5df86b5e1401b78d194e85
kacansky <57094731+kacansky@users.noreply.github.com>
2021-06-30 15:48:27 +0200

    Zapocet home page - Front end

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+
+const Footer = () => {
+    return (
+        <Fragment>
+             <footer className="py-1">
+                <p className="text-center mt-1">
+                    Oktopod Shop - Prodajemo samo slatke oktopode
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/layout/MetaData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+export const MetaData = ({title}) => {
+    return (
+        <Helmet>
+            <title>{'Oktopod Shop'}</title>
+        </Helmet>
+    )
+}
